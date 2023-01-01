c1a50c6d7d1c0a9828fb70fbbf45200c02258a37
prabhat328 <116557871+prabhat328@users.noreply.github.com>
2023-01-01 11:29:45 +0530

    Update and rename degree.py to x^n(power).py

--- x^n(power).py
+++ x^n(power).py
@@ -1,9 +1,9 @@
 def power(base, power):
     number = 1
-    for i in range(1, power + 1):
+    for i in range(1, (power + 1)):
         number = number * base
         # print(number)
     return number 
 
-
+# power(10,5)
 print(power(10, 5))
