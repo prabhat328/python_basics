a553ca8fb8d4e984de925377c6fd2b52819f2a35
prabhat328 <116557871+prabhat328@users.noreply.github.com>
2023-11-01 15:05:39 +0530

    Update strong_no.py

--- strong_no.py
+++ strong_no.py
@@ -1,11 +1,12 @@
-num = int(input("Enter a no. to check if it is a strong no.:\n"))
-og_num = num
-fact = 0
 def factorial(n):
     if n == 1:
         return 1
     else:
         return (n * factorial(n-1))
+
+num = int(input("Enter a no. to check if it is a strong no.:\n"))
+og_num = num
+fact = 0
 
 while(num >0):
     fact = fact + factorial(num%10)
