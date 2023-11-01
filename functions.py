ad10ddf10738da0838c3d4ab7f93fb39b558253a
prabhat328 <116557871+prabhat328@users.noreply.github.com>
2023-11-01 20:58:10 +0530

    Update and rename practical2.py to functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,4 @@
-# Pract 2
-
-# QA
+# A
 a = 5.76345
 b = 30
 # 1
@@ -19,7 +17,7 @@ print(init)
 ran = range(a,b)
 print(ran)
 
-# QB
+# B
 def vol_sphere(r):
     volume = (4/3) * 3.14 * (r ** 3)
     print(volume)
@@ -28,7 +26,7 @@ def vol_sphere(r):
 radius = int(input("Enter radius of sphere to get it`s volume\n"))
 vol_sphere(radius)
 
-# QC
+# C
 def factorial(a):
     if a == 0 or a ==1:
         return 1
