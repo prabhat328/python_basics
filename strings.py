4cc003a4e45bad9b74316dc67635a499fbd367ea
prabhat328 <116557871+prabhat328@users.noreply.github.com>
2023-11-01 20:57:27 +0530

    Update and rename practical3.py to strings.py

--- strings.py
+++ strings.py
@@ -1,17 +1,16 @@
-# Prac 3
 
-# QA
+# A
 str = "Hello World" # initialising
 print(str[2:7]) # slicing
 print(str * 2) # repetition
 print(str + "Python") # concatenation
 
-# QB
+# B
 str2 = "morning"
 for i in str2:
     print(i)
 
-# QC
+# C
 a = 0
 for i in "hippopotamus":
     if i == 'p':
